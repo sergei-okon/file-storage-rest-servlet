7696254aeaec80f407cff728c7687bded915785a
sergei-okon <sergei.okon@gmail.com>
2022-04-11 20:42:10 +0300

    Added tests

--- src/main/resources/db.migration/V2__inser_dt_todb.sql
+++ src/main/resources/db.migration/V2__inser_dt_todb.sql
@@ -1,11 +1,11 @@
-INSERT INTO files_storage_servlet.files (file_name, path)
+INSERT INTO files (file_name, path)
 VALUES ('hibernate.doc', 'c:\java'),
        ('flyway.doc', 'c:\java\course'),
        ('heroku.doc', 'c:\course\db'),
        ('account.xls', 'c:\english\db'),
        ('ticket.doc', 'c:\java\db');
 
-insert into files_storage_servlet.users (name)
+insert into users (name)
 values ('Jevelin'),
        ('Bayraktar'),
        ('Peter'),
@@ -14,7 +14,7 @@ values ('Jevelin'),
        ('Sergei'),
        ('Oleg');
 
-insert into files_storage_servlet.events (created, updated, file_id, user_id)
+insert into events (created, updated, file_id, user_id)
 values ('2022-03-08 12:19:03.000000', '2022-03-08 12:19:06.000000', 1, 1),
        ('2022-03-12 12:19:03.000000', '2022-03-12 12:19:06.000000', 2, 2),
        ('2022-03-10 12:10:03.000000', '2022-03-10 12:19:06.000000', 3, 1),
